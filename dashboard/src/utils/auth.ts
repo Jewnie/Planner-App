7dd085a873d8f15c491d60179f591f865792b155
Jonathan Loore <jonathan.loore@gmail.com>
2025-11-11 18:14:04 +0100

    feat add google to better-auth

--- dashboard/src/utils/auth.ts
+++ dashboard/src/utils/auth.ts
@@ -5,4 +5,10 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
+    socialProviders: { 
+        google: { 
+          clientId: process.env.GOOGLE_CLIENT_ID as string, 
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 });

466b8d98f0860880e793d48cd2bb47984b1b1b4d
Jonathan Loore <jonathan.loore@gmail.com>
2025-11-13 17:13:27 +0100

    fix db.ts pg pool

--- core/src/db.ts
+++ core/src/db.ts
@@ -5,6 +5,9 @@ import * as schema from "./db/schema.js"
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
+  ssl: process.env.DATABASE_URL?.includes("localhost")
+    ? undefined
+    : { rejectUnauthorized: false },
 })
 
 export const db = drizzle(pool, { schema })

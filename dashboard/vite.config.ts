8dc997bf702212ff1804284b8660b2a8d12c574a
Jonathan Loore <jonathan.loore@gmail.com>
2025-11-14 16:33:36 +0100

    fiiiixxx plessss

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  base: "/", // IMPORTANT for Vercel to find assets correctly
+  build: {
+    outDir: "dist", // make sure build output goes to dashboard/dist
+    assetsDir: "assets", // optional: where JS/CSS will be placed
   },
 });

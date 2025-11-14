894838184190f1b81dda6b4d6372b22a54c7eeaa
Jonathan Loore <jonathan.loore@gmail.com>
2025-11-14 09:42:54 +0100

    credentials include

--- dashboard/src/lib/auth-client.ts
+++ dashboard/src/lib/auth-client.ts
@@ -2,6 +2,8 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: "https://planner-app-tau.vercel.app", // backend url
-     
+    fetchOptions: {
+        credentials: "include",  // REQUIRED
+      },
 })
 

155581688e39cf8eb52d5f805d5e94e4e435b9ab
Jonathan Loore <jonathan.loore@gmail.com>
2025-11-14 09:18:22 +0100

    test running auth with hardcode

--- core/src/auth.ts
+++ core/src/auth.ts
@@ -6,40 +6,38 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "./db.js"
 import * as schema from "./db/schema.js"
 
-const port = Number(process.env.PORT) || 3000
-const baseURL = process.env.AUTH_URL ?? `http://localhost:${port}`
+
+// const baseURL = process.env.AUTH_URL // points to backend url
 const googleClientId = process.env.GOOGLE_CLIENT_ID
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
 
-const frontendOrigin = process.env.FRONTEND_URL
-  ? process.env.FRONTEND_URL.replace(/\/$/, "")
-  : undefined
 
 export const auth = betterAuth({
-  baseURL,
+  baseURL: "https://planner-app-tau.vercel.app",
+
   trustedOrigins: [
-    baseURL,
-    "http://localhost:5173",
-    ...(frontendOrigin ? [frontendOrigin] : []),
+    "https://planner-app-tau.vercel.app"
   ],
+
   socialProviders: googleClientId && googleClientSecret ? {
     google: {
       clientId: googleClientId,
       clientSecret: googleClientSecret,
     },
   } : undefined,
+
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
   }),
+
   cookies: {
     session: {
       secure: true,
-      sameSite: "none",  // import: MUST be none for cross-domain
-      domain: baseURL,
+      sameSite: "none",
+      // ❌ NO domain here
     },
   },
-  
-  plugins: [],
-})
 
+  plugins: [],
+});
--- dashboard/src/lib/auth-client.ts
+++ dashboard/src/lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: import.meta.env.VITE_AUTH_URL ?? "http://localhost:3000",
-    
+    baseURL: "https://planner-app-tau.vercel.app", // backend url
+     
 })
+
